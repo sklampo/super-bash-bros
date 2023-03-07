01eda9c6387555beb5d22efc144da3f03ad33cf9
Stephen Kirkpatrick <39313703+sklampo@users.noreply.github.com>
2023-03-07 09:57:07 -0600

    feat: add ANSI color escape codes and PS1 prompt (#29)

--- shell/index.sh
+++ shell/index.sh
@@ -18,11 +18,14 @@ finish_setup ()
 
   (return 0 2>/dev/null) || only_sourcing_allowed
 
-  ALIASES_ROOT="${SCRIPTS_ROOT}/aliases"
-  FUNCTIONS_ROOT="${SCRIPTS_ROOT}/functions"
+  source_index "${SCRIPTS_ROOT}/aliases"
+  source_index "${SCRIPTS_ROOT}/functions"
+}
 
-  source_index "${ALIASES_ROOT}"
-  source_index "${FUNCTIONS_ROOT}"
+prompt_setup ()
+{
+  # shellcheck source=/shell/prompt/primary
+  source "${SCRIPTS_ROOT}/prompt/primary"
 }
 
 running_shell=$(ps -o command -p $$ | sed 1d)
@@ -30,10 +33,12 @@ case "${running_shell}" in
   *bash)
     SCRIPTS_ROOT=$(dirname "${BASH_SOURCE[0]}")
     finish_setup
+    prompt_setup
     ;;
   *zsh)
     SCRIPTS_ROOT=$(dirname "${0}")
     finish_setup
+    prompt_setup
     ;;
 esac
 
